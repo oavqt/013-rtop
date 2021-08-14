c944a8b8c72373ef6bcf0cc71692a80d22fd16db
oav <ortizqtv@gmail.com>
2021-08-13 23:03:38 -0500

    Init

---
+++ src/server/express.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const server = express();
+const webpack = require('webpack');
+const config = require('../../webpack.config');
+const compiler = webpack(config);
+
+const webpackDevMiddleWare = require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.pubilcPath,
+});
+const webpackHotMiddleWare = require('webpack-hot-middleware')(compiler, {
+  reload: true,
+});
+const staticMiddleWare = express.static('dist');
+
+server.use(webpackDevMiddleWare);
+server.use(webpackHotMiddleWare);
+server.use(staticMiddleWare);
+
+server.listen(3333, () => {
+  console.log('Server is starting...');
+});
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: development,
+  entry: {
+    main: ['./src/index.js', 'webpack-hot-middleware/client'],
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    assetModuleFilename: 'images/[hash][ext][query]',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gir)$/i,
+        use: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HTMLWebpackPlugin({
+      title: 'Le Temp',
+    }),
+    new webpack.SourceMapDevToolPlugin({
+      test: /\.s?[as]ss?/i,
+    }),
+    new webpack.EvalSourceMapDevToolPlugin({
+      test: /\.(vue|[jt]sx?)$/i,
+    }),
+  ],
+};
