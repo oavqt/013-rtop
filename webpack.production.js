401a5354a94b4447143c2d6f79eed11d30875af7
oav <ortizqtv@gmail.com>
2021-08-23 08:39:30 -0500

    Improve the performance by compressing the images

--- webpack.production.js
+++ webpack.production.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
 const theBaseConfig = require('./webpack.base');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 const theProductionConfig = {
   mode: 'production',
@@ -11,11 +11,21 @@ const theProductionConfig = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gir)$/i,
+        type: 'asset',
+      },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'css/[name].css',
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [['optipng', { optiminizationLevel: 5 }]],
+      },
     }),
   ],
 };
